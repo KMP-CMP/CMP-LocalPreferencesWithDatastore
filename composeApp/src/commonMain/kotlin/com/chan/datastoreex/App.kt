eeb579663360896c0721edd88acac95cac35bf81
윤찬 <vsvx13@naver.com>
2025-09-28 21:06:11 +0900

    [feat]: UI 구현 & dataStore 파라미터 주입 및 counter 사용

--- composeApp/src/commonMain/kotlin/com/chan/datastoreex/App.kt
+++ composeApp/src/commonMain/kotlin/com/chan/datastoreex/App.kt
@@ -1,11 +1,9 @@
 package com.chan.datastoreex
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -13,36 +11,50 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import datastoreex.composeapp.generated.resources.Res
-import datastoreex.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
-fun App() {
+fun App(
+    prefs: DataStore<Preferences>
+) {
+    val counterKey = intPreferencesKey("counter")
+    val counter by prefs.data
+        .map { it[counterKey] ?: 0 }
+        .collectAsState(0)
+
+    val scope = rememberCoroutineScope()
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
         Column(
             modifier = Modifier
                 .background(MaterialTheme.colorScheme.primaryContainer)
                 .safeContentPadding()
                 .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+            Text(
+                text = counter.toString(),
+                textAlign = TextAlign.Center,
+                fontSize = 50.sp
+            )
+
+            Button(onClick = {
+                scope.launch {
+                    prefs.edit { pref ->
+                        pref[counterKey] = counter + 1
+                    }
                 }
+            }) {
+                Text("Increment!")
             }
         }
     }
