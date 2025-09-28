4013b0386292727180589e74b9b611f8ed156b73
윤찬 <vsvx13@naver.com>
2025-09-28 21:06:26 +0900

    [feat]: 각 OS별 DataStore 주입하기

--- composeApp/src/iosMain/kotlin/com/chan/datastoreex/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/chan/datastoreex/MainViewController.kt
@@ -1,5 +1,12 @@
 package com.chan.datastoreex
 
+import androidx.compose.runtime.remember
 import androidx.compose.ui.window.ComposeUIViewController
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController {
+    App(
+        prefs = remember {
+            createDataStore()
+        }
+    )
+}
--- composeApp/src/jvmMain/kotlin/com/chan/datastoreex/main.kt
+++ composeApp/src/jvmMain/kotlin/com/chan/datastoreex/main.kt
@@ -1,13 +1,22 @@
 package com.chan.datastoreex
 
+import androidx.compose.runtime.remember
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
-fun main() = application {
-    Window(
-        onCloseRequest = ::exitApplication,
-        title = "DataStoreEx",
-    ) {
-        App()
+fun main() {
+    val prefs = createDataStore {
+        DATA_STORE_FILE_NAME
+    }
+
+    application {
+        Window(
+            onCloseRequest = ::exitApplication,
+            title = "DataStoreEx",
+        ) {
+            App(
+                prefs = prefs
+            )
+        }
     }
 }
